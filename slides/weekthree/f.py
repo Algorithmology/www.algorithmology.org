95e6eb60646659f24d44a710e5f3316fcd633bfd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-06 17:10:02 -0500

    fix: Move functions to a separate file for slides/weekthree/f.py.

--- slides/weekthree/f.py
+++ slides/weekthree/f.py
@@ -1,10 +1,7 @@
-"""f.py: A module to benchmark Python function call overhead.
-"""
+"""f.py: A module to benchmark Python function call overhead."""
+
 
 def f():
-    pass
-
-def g(*args, **kwargs):
     pass
 
 
@@ -19,29 +16,9 @@ def loop_1_f(n):
     for i in range(n):
         f()
 
+
 def loop_2_f_twice(n):
     """Run loop calling empty function twice per loop, n times."""
     for i in range(n):
         f()
         f()
-
-def loop_3_g(n):
-    """Run loop with empty function taking args, n times."""
-    for i in range(n):
-        g()
-
-def loop_4_g_twice(n):
-    """Run loop with empty function taking args, twice per loop, n times."""
-    for i in range(n):
-        g()
-        g()
-
-def loop_5_g_arg(n):
-    """Run loop with empty function passing an arg, n times."""
-    for i in range(n):
-        g(n)
-
-def loop_6_g_kwarg(n):
-    """Run loop with empty function passing a kwarg, n times."""
-    for i in range(n):
-        g(n=n)
